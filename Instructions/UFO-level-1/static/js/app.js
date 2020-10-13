0e8c27b2455633dd86a69caccf96a1fb5fce2f57
GiamSigaud <giam_sigaud@hotmail.com>
2020-10-12 21:02:29 -0500

    10.12.20 9:02pm CST Part 1 Complete - Just need some clean up

--- Instructions/UFO-level-1/static/js/app.js
+++ Instructions/UFO-level-1/static/js/app.js
@@ -2,25 +2,28 @@
 let varData = data;
 
 // have d3 select the table
-let table = d3.select('table');
-
-// have d3 create a striped table
-table.attr('class', "table table-striped");
+//let table = d3.select('table');
 
 // have d3 select the table body
 let tbody = d3.select('tbody');
 
+varData.forEach(addRow);
+
+
+function addRow(item){
+ var row = tbody.append('tr');
+ var result = Object.values(item);
+ result.forEach(data=>{
+   row.append('td').text(data)
+ })
+
+}
+
+
 // add a row to the table body
-let row = tbody.append('tr');
+// let row = tbody.append('tr');
 
 // add a value to the column cell
-row.append('td').text(varData[0]);
-row.append('td').text(varData[1]);
-row.append('td').text(varData[2]);
-row.append('td').text(varData[3]);
-row.append('td').text(varData[4]);
-row.append('td').text(varData[5]);
-row.append('td').text(varData[6]);
 
 
 
@@ -37,38 +40,42 @@ row.append('td').text(varData[6]);
 
 
 
-// Assign the data from `data.js` to a descriptive variable
-// let tableData = data1;
+// Select the button
+let button = d3.select("#filter-btn");
 
-// // Select the button
-// let button = d3.select("#filter-btn");
+// Select the form
+//let form = d3.select("#filters");
 
-// // Select the form
-// let form = d3.select("#filters");
+// Create event handlers 
+button.on("click", runEnter);
+//form.on("submit",runEnter);
 
-// // Create event handlers 
-// button.on("click", runEnter);
-// form.on("submit",runEnter);
+// Complete the event handler function for the form
+function runEnter() {
 
-// // Complete the event handler function for the form
-// function runEnter() {
+  // Prevent the page from refreshing
+  // keeps the page from refreshing every time i press the "Enter" key
+  d3.event.preventDefault();
+  tbody.html('')
+  // Select the input element and get the raw HTML node
+  let inputElement = d3.select("#datetime");
+  // let inputElement2 = d3.select("#city");
+  // Get the value property of the input element
+  let inputValue = inputElement.property('value');
+  // let inputValue2 = inputElement2.property('value');
 
-//   // Prevent the page from refreshing
-//   // keeps the page from refreshing every time i press the "Enter" key
-//   d3.event.preventDefault();
+  // console.log(inputValue2);
+  console.log(varData);
 
-//   // Select the input element and get the raw HTML node
-//   let inputElement = d3.select("#ufo-sighting-form-input");
+  // Use the form input to filter the data by blood type
 
-//   // Get the value property of the input element
-//   let inputValue = inputElement.property('value');
+  let filteredData = varData.filter(sighting => sighting.datetime === inputValue ); //&& sighting.city == inputValue2
+  filteredData.forEach(addRow)
+  console.log(filteredData);
+};  
 
-//   console.log(inputValue);
-//   console.log(tableData);
 
-//   // Use the form input to filter the data by blood type
-//   let filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+//filteredData.forEach(addRow)
 
-//   console.log(filteredData);
-// };  
+
 
